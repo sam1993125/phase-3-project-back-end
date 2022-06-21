b725146cb06927484bc3d580871931ffe1e7aff6
Em <ekramm@me.com>
2022-06-21 12:08:00 -0400

    started creating get requests

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -5,5 +5,16 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+  get "/question/:id" do
+    questions = Question.find(params[:id])
+    questions.to_json
+  end
+  get "/question" do
+    all_questions = Question.all
+    all_questions.to_json
+  end
+
+  
+
 
 end
