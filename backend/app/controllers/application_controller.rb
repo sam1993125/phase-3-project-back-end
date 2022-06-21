5eea591f22a8274829a820acc7cd48b1bf14804c
Yixin Gong <sam19931225@gmail.com>
2022-06-21 14:02:04 -0400

    get request

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -2,19 +2,22 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
-  get "/question/:id" do
-    questions = Question.find(params[:id])
-    questions.to_json
-  end
+
   get "/question" do
     all_questions = Question.all
-    all_questions.to_json
+    all_questions.to_json 
+  end
+  
+  get "/question/:id" do
+    questions = Question.find(params[:id])
+    questions.to_json(include: { answers: { include: :likes} })
   end
 
-  
 
+  
 
 end
